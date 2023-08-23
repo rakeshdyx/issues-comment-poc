f6462da903b60dedcf9181b2135b998abab0d895
stackafflux <122041782+stackafflux@users.noreply.github.com>
2023-08-24 00:42:45 +0530

    with json vaule

--- .github/scripts/extract.py
+++ .github/scripts/extract.py
@@ -15,40 +15,20 @@ repo = g.get_repo("rakeshdyx/issues-comment-poc")
 
 issue = repo.get_issue(number=2)
 
+## Get the issue comment
 comments = issue.get_comments()
+
+## Get Body of the comment as json
 for comment in comments:
     comment_body_json = comment.body
+
+## Convert json to dictionary
 try:
     comment_body_dict = json.loads(comment_body_json)
-    print(comment_body_dict)
 except json.JSONDecodeError as e:
     print("Error decoding JSON", e)
 
+for key, value in comment_body_dict:
+    print(f"{key} -> {value}")
 
 
-# # Replace with your repository and issue or pull request number
-# repository_name = "saichitikeshi123/bayer-int/calantic-vm-deploy"
-# issue_number = 25
-
-# # Connect to GitHub using your token
-# g = Github(github_token)
-# repo = g.get_repo(repository_name)
-# issue = repo.get_issue(issue_number)
-
-# # Fetch comments from the issue
-# comments = issue.get_comments()
-
-# print(comments)
-
-# # Define the key you're looking for
-# target_key = "value"
-
-# # Search for the key in comments and extract the associated value
-# for comment in comments:
-#     comment_body = comment.body
-#     key_index = comment_body.find(f"@{target_key}:")
-#     if key_index != -1:
-#         start_index = key_index + len(target_key) + 2
-#         value = comment_body[start_index:].strip()
-#         print(f"Value for {target_key}: {value}")
-#         break  # Stop after finding the first instance
