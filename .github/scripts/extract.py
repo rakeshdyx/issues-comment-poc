d1f8f05447f7b83139b5ab5584cbe3230ae5ffb1
stackafflux <122041782+stackafflux@users.noreply.github.com>
2023-08-24 01:29:19 +0530

    update secrets

--- .github/scripts/extract.py
+++ .github/scripts/extract.py
@@ -2,16 +2,41 @@ from github import Github
 from github import Auth
 import sys
 import json
+import requests
+import base64
 
 
-# Replace with your GitHub token or credentials
+def createSecret(repository_name, secret_name, secret_value, github_token):
+    api_url = f"https://api.github.com/repos/{repository_name}/actions/secrets/{secret_name}"
+    headers = {
+    "Authorization": f"Bearer {github_token}",
+    "Accept": "application/vnd.github.v3+json"
+    }
+### Encode the secret value as base64
+    encoded_secret_value = base64.b64encode(secret_value.encode()).decode()
+### API request payload
+    payload = {
+        "encrypted_value": encoded_secret_value,
+        "key_id": ""
+        }
+    response = requests.put(api_url, json=payload, headers=headers)
+
+    if response.status.code == 201:
+        print(f"Secret '{secret_name}' created successfully.")
+    else:
+        print(f"Failed to create secret '{secret_name}'.")
+        print(f"Response status code: {response.status_code}")
+        print(f"Response content: {response.content}")
+
 github_token = sys.argv[1]
+repo_name = "rakeshdyx/issues-comment-poc"
+# Replace with your GitHub token or credentials
 
 auth = Auth.Token(github_token)
 
 g = Github(auth=auth)
 
-repo = g.get_repo("rakeshdyx/issues-comment-poc")
+repo = g.get_repo(repo_name)
 
 issue = repo.get_issue(number=2)
 
@@ -30,11 +55,11 @@ except json.JSONDecodeError as e:
 
 for key, value in comment_body_dict.items():
     if key == "Job":
-        print("Update Github Secrets Job Name")
+        createSecret(repo_name, "JOB_NAME", value, github_token)
     elif key == "InstanceName":
-        print("Update Github Secrets InstanceName")
+        createSecret(repo_name, "JOB_NAME", value, github_token)
     elif key == "Environment":
-        print("Update Github Secrets for ENV")
+        createSecret(repo_name, "JOB_NAME", value, github_token)
     else:
         print("Value is unrecognized, Please validate input data")
 
